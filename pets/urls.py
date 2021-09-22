248e757a3616c483f3183aa9dbf95785b55d4ddd
patgarcia <commonzenmedia@gmail.com>
2021-09-22 14:43:30 -0400

    Breed html templates and fixture

--- pets/urls.py
+++ pets/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import BreedsListView
+from .views import BreedsListView, BreedsDetailView
 urlpatterns = [
-  path('', BreedsListView.as_view(), name="breed_list")
+  path('', BreedsListView.as_view(), name="breed_list"),
+  path('<uuid:pk>', BreedsDetailView.as_view(), name="breed_detail")
 ]
--- pets/views.py
+++ pets/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Breed
 
 # Create your views here.
 
 
 class BreedsListView(ListView):
+    model = Breed
+
+class BreedsDetailView(DetailView):
     model = Breed
