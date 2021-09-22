be5f254af6d3b48ea0e467030edecf9cf2c40d9f
patgarcia <commonzenmedia@gmail.com>
2021-09-22 15:38:50 -0400

    Installed djangorestframework

--- helpers/breeds_fixtures.py
+++ helpers/breeds_fixtures.py
@@ -1,6 +1,9 @@
 import requests
 import json
 
+# Fixture commands
+# https://stackoverflow.com/a/49941917/5727431
+
 # https://dog.ceo/api/breed/australian/shepherd/images/random
 # https://dog.ceo/api/breeds/list/all
 API_URL = "https://dog.ceo/api"
@@ -9,21 +12,31 @@ RAND_IMG = "images/random"
 
 response = requests.get(API_URL + "/breeds/list/all")
 json_data = json.loads(response.text)
-breeds_data = json_data['message']
+breeds_data = json_data["message"]
 
 main_breeds = list(breeds_data.keys())
 data = []
 
-def get_image_and_create_object(breeds, parent=''):
-  for breed in breeds:
-    img_url = API_URL + IMG_URL + parent + ('/' if parent else '') + breed + '/' + RAND_IMG
-    print(img_url)
-    response = requests.get(img_url)
-    if(response.status_code == 200):
-      img_url = json.loads(response.text)['message']
-      dog = {'breed': breed, 'parent': parent, 'img_url': img_url }
-      data.append(dog)
-    if (not parent and breeds_data[breed]):
-      get_image_and_create_object(breeds_data[breed], breed)
+
+def get_image_and_create_object(breeds, parent=""):
+    for breed in breeds:
+        img_url = (
+            API_URL
+            + IMG_URL
+            + parent
+            + ("/" if parent else "")
+            + breed
+            + "/"
+            + RAND_IMG
+        )
+        print(img_url)
+        response = requests.get(img_url)
+        if response.status_code == 200:
+            img_url = json.loads(response.text)["message"]
+            dog = {"breed": breed, "parent": parent, "img_url": img_url}
+            data.append(dog)
+        if not parent and breeds_data[breed]:
+            get_image_and_create_object(breeds_data[breed], breed)
+
 
 get_image_and_create_object(main_breeds)
