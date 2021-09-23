7427cbdf9b048394df807951528ac101216fa196
patgarcia <commonzenmedia@gmail.com>
2021-09-23 03:29:04 -0400

    Extended Breed serializer to include parent name

--- api/serializers.py
+++ api/serializers.py
@@ -3,7 +3,15 @@ from rest_framework import serializers
 from pets.models import Breed
 
 
+class SubBreedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Breed
+        fields = ("name", "id")
+
+
 class BreedSerializer(serializers.ModelSerializer):
+    parent = SubBreedSerializer(read_only=True)
+
     class Meta:
         model = Breed
         fields = ("name", "img_url", "id", "parent")
